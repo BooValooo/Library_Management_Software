39d2543357bea784d23647e021267cd7fc1b5a14
Valentin Abgrall <valentin.abgrall@student-cs.fr>
2023-10-23 22:39:21 +0200

    styling fonctionnel pour l'authentification

--- src/main/java/com/example/bibliotheque/MainApplication.java
+++ src/main/java/com/example/bibliotheque/MainApplication.java
@@ -11,14 +11,13 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("authView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        scene.getStylesheets().add("");
+        Scene scene = new Scene(fxmlLoader.load(), 360, 400);
+        scene.getStylesheets().add(MainApplication.class.getResource("styles.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args) {launch();
     }
 }
